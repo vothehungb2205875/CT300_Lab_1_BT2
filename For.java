b552219dba7863fc2011e0da0fd3c1e5cf9b39c6
vothehungb2205875 <hungb2205875@student.ctu.edu.vn>
2025-05-23 19:41:17 +0700

    tao file For.java

---
+++ For.java
@@ -0,0 +1,10 @@
+package BaiTap_2;
+
+public class For {
+    public static void main(String[] args) {
+        // In ra các số từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
